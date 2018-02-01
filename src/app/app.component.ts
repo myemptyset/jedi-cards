343312d2277fa0bd98df6e79972bd271e745fd30
Anna Monros <minerva33@gmail.com>
2018-02-01 21:00:14 +0100

    Info not needed in this component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,39 +9,5 @@ import SocialLinks from './_models/social-links.model';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  // title = 'my app';
-  aboutProject: AboutProject = new AboutProject(
-    'Jedi Card Api',
-    'The project from the Angular',
-    'I am the description'
-  );
 
-  // social: SocialLinks = new SocialLinks(
-  //   'Twitter',
-  //   'twitter.com/emptyset'
-  // );
-
-  // test: Array<SocialLinks> = this.social;
-
-  author: Author = new Author(
-    'Anna',
-    'Monros',
-    'It is me',
-    [
-      new SocialLinks('Twitter', 'twitter.com/emptyset'),
-      new SocialLinks('Twitter', 'twitter.com/emptyset')
-    ]
-  );
-
-  getFullAuthorName() {
-    return this.author.name + ' ' + this.author.lastname;
-  }
-
-  // aboutProject: any = {
-  //   title: "Jedi cards"
-  // } ho podem fer així, sense crear el model però no fa autocompletar
-
-  // onButtonClicked() {
-  //   this.title = "you've clicked the button";
-  // }
 }
